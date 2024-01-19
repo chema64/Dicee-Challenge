41c3952ca6cabe37fad42d8f5e640907687259cf
chema64 <chema64@gmail.com>
2024-01-19 13:13:10 +1000

    Boss Level Challenge 1 - The Dicee Game

---
+++ index.js
@@ -0,0 +1,17 @@
+var randomNumber1 = 0;
+var randomNumber2 = 0;
+randomNumber1 = Math.random() * 6;
+randomNumber1 = Math.floor(randomNumber1) + 1;
+randomNumber2 = Math.random() * 6;
+randomNumber2 = Math.floor(randomNumber2) + 1;
+// alert(randomNumber1);
+document.querySelector(".img1").setAttribute("src", "images/dice"+randomNumber1+".png");
+document.querySelector(".img2").setAttribute("src", "images/dice"+randomNumber2+".png");
+
+if(randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML ="🚩 Player 1 Wins!"
+    }else if(randomNumber2 > randomNumber1) {
+            document.querySelector("h1").innerHTML ="Player 2 Wins! 🚩"
+        }else if(randomNumber1 === randomNumber2) {
+            document.querySelector("h1").innerHTML ="🚩 Draw! 🚩"
+        }
